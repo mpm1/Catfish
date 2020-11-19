dc2e0b56a2fd8e9302eda263a1cc839141011721
Thomas Welz <twelz@ualberta.ca>
2020-11-19 15:14:00 -0700

    redo last change since it was offline during commit-push for page indexing changes

--- Catfish/Services/PageIndexingService.cs
+++ Catfish/Services/PageIndexingService.cs
@@ -73,7 +73,7 @@ namespace Catfish.Services
             }
 
             //For given block is an ImageBlock or any specialization of it,
-            //then we index its Url
+            //then we index its Url  - need to remove leading tilde for now
             if (typeof(ImageBlock).IsAssignableFrom(block.GetType()))
             {
                 string text = (block as ImageBlock).Body.Media.PublicUrl.TrimStart('~');
